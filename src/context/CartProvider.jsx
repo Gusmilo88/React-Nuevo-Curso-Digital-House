d0a39a778415287d9b054145090eb834f6262f86
Gus <gus_eze1988@hotmail.com>
2023-07-13 16:26:04 -0300

    clase 5 terminada

--- src/context/CartProvider.jsx
+++ src/context/CartProvider.jsx
@@ -1,23 +1,12 @@
-import { createContext, useState } from "react"
+import { createContext } from "react"
 import PropTypes from 'prop-types'
-import useDrinks from "../hooks/useDrinks";
 
 const CartContext = createContext(null)
 
 const CartProvider = ({children}) => {
 
-    const [cart, setCart] = useState([])
-
-    const {drinks} = useDrinks()
-
-    const addCart = (idDrink) => {
-        const drinkItemCart = drinks.find(drink => drink.idDrink === idDrink)
-        setCart([...cart, drinkItemCart])
-    }
-
     const contextValues = {
-        cart,
-        addCart
+        
     }
 
   return (
